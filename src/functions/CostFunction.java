18d4989c88c6bc7763ed7436ec69df4cc0939c39
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2018-09-30 08:59:12 -0400

    Implemented MSE Cost Function
     - Completed and tested the meanSquaredError function (MSE)

--- src/functions/CostFunction.java
+++ src/functions/CostFunction.java
@@ -4,8 +4,25 @@ import qsor.Vector;
 
 public class CostFunction {
 	
-	public static double meanSquaredError (Vector v) {
-		return -1;
+	public static double MSE (Vector p, Vector t) {
+		// Mean Squared Error cost function
+		
+		if (p.size() != t.size()) {
+			throw new IllegalArgumentException("The predictions and truth vectors must be the same size " 
+												+ p.size() + " != " + t.size());
+		}
+		
+		double[] predictions = p.getValue();
+		double[] truth = t.getValue();
+		
+		int m = t.size();
+		double error = 0;
+		
+		for (int i=0; i<m; i++) {
+			error += Math.sqrt(truth[i] - predictions[i]);
+		}
+		
+		return error/m;
 	}
 	
 }
